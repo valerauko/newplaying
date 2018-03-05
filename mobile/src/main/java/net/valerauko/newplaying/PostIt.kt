1e05668e7d3b6239c20dec8e5ddf27fcc5085eb9
Unknown <valerauko@gmail.com>
2018-03-06 00:23:06 +0900

    Allow enabling/disabling

--- mobile/src/main/java/net/valerauko/newplaying/PostIt.kt
+++ mobile/src/main/java/net/valerauko/newplaying/PostIt.kt
@@ -11,10 +11,22 @@ import java.io.File
  */
 class PostIt: BroadcastReceiver() {
     private val tag = "PostIt"
-    override fun onReceive(context: Context?, intent: Intent?) {
+
+    private var twitter = true
+    private var mastodon = true
+
+    override fun onReceive(context: Context, intent: Intent?) {
         Log.i(tag, "Received post request.")
+        canPost(context)
         val message = "#nowplaying ${CurrentTrack.track}"
-        PostTwitter(TwitterData()).execute(message)
-        PostMastodon(MastodonData()).execute(message)
+        if(twitter) PostTwitter(TwitterData()).execute(message)
+        if(mastodon) PostMastodon(MastodonData()).execute(message)
+    }
+
+    // TODO: refactor to an abstract Poster class
+    private fun canPost(ctx: Context) {
+        val prefs = ctx.getSharedPreferences("net.valerauko.newplaying.SETTINGS", Context.MODE_PRIVATE)
+        twitter = prefs.getBoolean("twitter",true)
+        mastodon = prefs.getBoolean("mastodon", true)
     }
 }
